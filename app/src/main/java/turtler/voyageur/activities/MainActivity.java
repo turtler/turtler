fc842ae6f75f32440b0e1cab75dd6156f1392065
Caroline Wong <carowong5@gmail.com>
2016-09-02 23:55:28 -0700

    Working renamed app and added nav bar

---
+++ app/src/main/java/turtler/voyageur/activities/MainActivity.java
@@ -0,0 +1,26 @@
+package turtler.voyageur.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import turtler.voyageur.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.bottom_nav_bar, menu);
+        return true;
+    }
+}
