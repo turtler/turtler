46e074437d663cbea0cd270c1ef00aae1e983cd0
Sukhada Kulkarni <skulkarni@hearsaycorp.com>
2016-09-11 16:01:13 -0700

    Upload to AWS when taking pictures with camera/from photo gallery

---
+++ app/src/main/java/turtler/voyageur/utils/Constants.java
@@ -0,0 +1,20 @@
+package turtler.voyageur.utils;
+
+/**
+ * Created by skulkarni on 9/11/16.
+ */
+public class Constants {
+
+    /*
+     * You should replace these values with your own. See the README for details
+     * on what to fill in.
+     */
+    public static final String COGNITO_POOL_ID = "us-west-2:27a72964-50ee-42ea-b27b-061931d77362";
+
+    /*
+     * Note, you must first create a bucket using the S3 console before running
+     * the sample (https://console.aws.amazon.com/s3/). After creating a bucket,
+     * put it's name in the field below.
+     */
+    public static final String BUCKET_NAME = "voyaging";
+}
