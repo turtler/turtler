a2fabce2b2cbbf844b980a33e33c6b45a3cacce3
Sukhada Kulkarni <skulkarni@hearsaycorp.com>
2016-09-17 15:00:19 -0700

    Add cover photo image to trip activity view

--- app/src/main/java/turtler/voyageur/fragments/ViewPagerContainerFragment.java
+++ app/src/main/java/turtler/voyageur/fragments/ViewPagerContainerFragment.java
@@ -3,14 +3,14 @@ package turtler.voyageur.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.astuetz.PagerSlidingTabStrip;
+import com.squareup.picasso.Picasso;
 
-import butterknife.BindView;
 import turtler.voyageur.R;
 import turtler.voyageur.adapters.TripFragmentPageAdapter;
 
@@ -18,20 +18,28 @@ import turtler.voyageur.adapters.TripFragmentPageAdapter;
  * Created by skulkarni on 9/14/16.
  */
 public class ViewPagerContainerFragment extends Fragment {
+    ImageView ivCover;
     PagerSlidingTabStrip tabsStrip;
     public ViewPager viewPager;
     TripFragmentPageAdapter pagerAdapter;
+    String tripImage;
     public ViewPagerContainerFragment() {   }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        tripImage = getArguments().getString("tripImage");
+
         View root = inflater.inflate(R.layout.fragment_view_pager_container, container, false);
 
         pagerAdapter = new TripFragmentPageAdapter(getActivity().getSupportFragmentManager());
         viewPager = (ViewPager) root.findViewById(R.id.viewPager);
+        ivCover = (ImageView) root.findViewById(R.id.ivCoverImage);
         tabsStrip = (PagerSlidingTabStrip) root.findViewById(R.id.tabStrip);
+        if (tripImage != null) {
+            Picasso.with(getContext()).load(tripImage).into(ivCover);
+        }
 
         viewPager.setAdapter(pagerAdapter);
         tabsStrip.setViewPager(viewPager);
